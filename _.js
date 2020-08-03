8dc7ba98ad72ece6f49a88c34590063ec6bbf317
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2020-08-02 21:06:23 -0400

    added .inRange() method to _ js object

--- _.js
+++ _.js
@@ -1,6 +1,36 @@
+const _ = {
 
+    clamp(number, lower, upper) {
+        let lowerClampedValue = Math.max(number, lower);
+        let clampedValue = Math.min(lowerClampedValue, upper);
+        return clampedValue;
+    },
 
+    inRange(number, start, end) {
+       if(typeof end == 'undefined'){
+            end = start;
+            start = 0;
+       }
+       //console.log(start + '/' + end);
+       if(start > end){
+            let temp = end;
+            end = start;
+            start = temp;
+        }
+        //console.log(start + '/' + end);
+        let isInRange = null;
+        if(number >= start && number < end){
+            isInRange = true;
+        }else {
+            isInRange = false;
+        }
+        return isInRange;
+    }, 
 
+    words() {
+
+    },
+};
 
 
 // Do not write or modify code below this line.
